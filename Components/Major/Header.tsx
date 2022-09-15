239d069a6b4def61d0f6c916bdc15d979157442f
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-09-15 13:24:21 +0100

    feat: added sidebar

--- Components/Major/Header.tsx
+++ Components/Major/Header.tsx
@@ -1,7 +1,25 @@
-import { Box } from "@chakra-ui/react";
+import { Box , Text } from "@chakra-ui/react";
 import { Logo } from "../Minors/Logo";
 import {FaBars} from 'react-icons/fa'
+import Sidebar from "./Sidebar";
+import { useState } from "react";
+
+
 const Header = () => {
+
+
+    let [ open , setOpen ] = useState(0)
+
+    //handle close 
+    const handleSidebar =()=> {
+        if( open === 0) { 
+            //@ts-ignore
+            setOpen('50%')
+         } else {
+            setOpen(0)
+         }
+    }
+
     return (
         <>
 
@@ -12,10 +30,12 @@ const Header = () => {
    
                 </Box>
 
-                <Box>
-                    <FaBars/> 
+                <Box onClick={handleSidebar} transition={'1s ease-in-out'}>
+                    {open === 0 ? <FaBars/>  : <Text fontSize={'2em'}>&times;</Text>}
                 </Box>
             </Box>
+
+            <Sidebar width={open}/>
         
         </>
     )
